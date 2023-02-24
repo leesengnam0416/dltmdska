0d62f5f34cf8d2b79532d985f4e8b52ef2066a7e
leeseungnam1234 <107826488+leeseungnam1234@users.noreply.github.com>
2023-02-24 16:13:19 +0900

    Add files via upload

---
+++ baeminlsnProject/MainPage/MainPage.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('#hFHbxs').children('div').css('display' , ' ');
+});
+
+$(function(){
+    $('#enArjT').children('ul').css({
+        'display' : ' ' ,
+        'list-style-type' : ' '
+    }),
+    $('#fNvXFi').children('a').css({
+        'color' : ' ' ,
+        'background-color' : ' '
+    }),
+    $('#fNvXFi').children('a:-webkit-any-link').css({
+        'color' : ' ',
+        'text-decoration' : ' '
+    });
+});
