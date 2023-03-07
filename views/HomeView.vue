cb42c47a4e8cc8cba20560a270124d6915c2654c
leeseungnam1234 <107826488+leeseungnam1234@users.noreply.github.com>
2023-03-08 01:27:06 +0900

    Add files via upload

---
+++ views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img class="logo" alt="Vue logo" src="../assets/baeminlogo.png">
+    <HelloWorld msg="어서오세요"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.logo {
+  width: 160px;
+  height: 70px;
+  float: left;
+}
+</style>
